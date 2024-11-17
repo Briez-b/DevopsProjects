a5354968af3f71cd58d603d592ac7a522563ae22
Yauheni Bryshten <zhenyabrishtenpl@gmail.com>
2024-11-17 23:17:31 +0100

    Commented out unecessary part

--- AWS/aws_lambda_snapshots/clean_snapshots.py
+++ AWS/aws_lambda_snapshots/clean_snapshots.py
@@ -7,12 +7,12 @@ def lambda_handler(event, context):
     response = ec2.describe_snapshots(OwnerIds=['self'])
 
     # Get all active EC2 instance IDs
-    instances_response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    active_instance_ids = set()
+    # instances_response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    # active_instance_ids = set()
 
-    for reservation in instances_response['Reservations']:
-        for instance in reservation['Instances']:
-            active_instance_ids.add(instance['InstanceId'])
+    #for reservation in instances_response['Reservations']:
+    #    for instance in reservation['Instances']:
+    #        active_instance_ids.add(instance['InstanceId'])
 
     # Iterate through each snapshot and delete if it's not attached to any volume or the volume is not attached to a running instance
     for snapshot in response['Snapshots']:
